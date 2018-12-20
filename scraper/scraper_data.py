9c8225249216768ea6169987e851bf6f25d57f3d
Trevor Xander <trevorcolexander@gmail.com>
2018-12-20 03:27:15 -0500

    Implement mutex on URL queue

--- scraper/scraper_data.py
+++ scraper/scraper_data.py
@@ -1,21 +1,28 @@
 import os
 import queue
+from threading import Lock
 class UrlList:
 
     _BUFFER_SIZE = 5000
-
+    mutex = Lock()
     def __init__(self, url_file_loc):
         self._buffer = queue.Queue(maxsize= UrlList._BUFFER_SIZE)
         self._file = open(url_file_loc, 'r+')
+
 
     def __iter__(self):
         return self
 
     def __next__ (self):
-        if self._buffer.empty():
-            self._reload_buffer()
-        new_url = self._buffer.get()
-        return new_url
+
+        try:
+            UrlList.mutex.acquire()
+            if self._buffer.empty():
+                self._reload_buffer()
+            new_url = self._buffer.get()
+            return new_url
+        finally:
+            UrlList.mutex.release()
 
     def _reload_buffer (self):
         for line in self._file:
