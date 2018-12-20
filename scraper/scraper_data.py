86f5edc71c885dd65e8d325412971701368f2968
Trevor Xander <trevorcolexander@gmail.com>
2018-12-20 13:11:23 -0500

    Implemented write buffer for scraped data

--- scraper/scraper_data.py
+++ scraper/scraper_data.py
@@ -1,20 +1,18 @@
 import os
 import queue
 from threading import Lock
-class UrlList:
 
+class UrlList:
     _BUFFER_SIZE = 5000
     mutex = Lock()
     def __init__(self, url_file_loc):
         self._buffer = queue.Queue(maxsize= UrlList._BUFFER_SIZE)
-        self._file = open(url_file_loc, 'r+')
-
+        self._file = open(url_file_loc, 'r')
 
     def __iter__(self):
         return self
 
     def __next__ (self):
-
         try:
             UrlList.mutex.acquire()
             if self._buffer.empty():
@@ -34,14 +32,54 @@ class UrlList:
             raise StopIteration
 
 
-
-
-
 class PageData:
 
-    def __init__(self, url, file_loc):
-        print('test pritn from scraper_data.py')
+    mutex = Lock()
 
+    def __init__(self,):
+        self._data = {}
+        self._data_index = 0
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._data_index > len(self._data) - 1:
+            raise StopIteration
+        new_data = self._data[self._data_index]
+        self._data_index += 1
+        return new_data
+
+    def add_data(self, data_desc , data):
+        self._data[data_desc] = data
+
+class PageDataCollection:
+    _MAX_PAGE_DATA = 10
+    mutex = Lock()
+
+    def __init__(self, write_file_loc):
+        self._buffer = queue.Queue(maxsize= PageDataCollection._MAX_PAGE_DATA)
+        self._file = open(write_file_loc, 'w')
+
+    def add_page (self, page_data: PageData):
+        try:
+            PageDataCollection.mutex.acquire()
+            self._buffer.put (page_data)
+            if self._buffer.full():
+                self.flush()
+        finally:
+            PageDataCollection.mutex.release()
+
+    def flush(self):
+        while not self._buffer.empty():
+            page_data: PageData
+            page_data = self._buffer.get()
+            self._file.writelines(page_data._data.get('title') + ['\n'])
+            self._file.writelines(page_data._data.get('url') + ['\n'])
+            self._file.writelines(page_data._data.get('description') + ['\n'])
+            words = page_data._data.get('words')
+            for word in words:
+                self._file.write(word + ' ')
+            self._file.write('\n\n')
 
 
