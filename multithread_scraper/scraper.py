7a94d895807e5ee13ee6899075a777be2f2fc4c7
Trevor Xander <trevorcolexander@gmail.com>
2018-12-24 12:54:36 +0530

    Made scraper class more robust

--- multithread_scraper/scraper.py
+++ multithread_scraper/scraper.py
@@ -1,49 +1,12 @@
-import threading
-import time
+from multithread_scraper import scraper_data
+
+import re
+import multithread_scraper.scraper_data
 from selenium import webdriver
 from selenium import common
-from threading import Lock
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
-from scraper.scraper_data import PageData
-from scraper.scraper_data import PageDataCollection
-
-class ScraperThread (threading.Thread):
-    data_collection = PageDataCollection("dataset/page_data.txt")
-    threadLock = Lock()
-    def __init__(self, driver_location, url):
-        threading.Thread.__init__(self)
-        self.driver_loc = driver_location
-        self.url = url
-        self.data = PageData()
-
-    def run(self):
-        try:
-            self.scraper = SearchScraper(self.driver_loc, self.url)
-            self.data.add_data("url", [self.url])
-            self.data.add_data('title', [self.scraper.get_title()])
-
-            self.data.add_data('description',self.scraper.get_description())
-            self.data.add_data('words', self.scraper.get_keywords())
-
-            sub_domains = self.scraper.get_links()
-            for url in sub_domains:
-                thread = ScraperThread(self.driver_loc, url)
-            self.data.add_data('links', sub_domains)
-
-            ScraperThread.threadLock.acquire()
-            ScraperThread.data_collection.add_page(self.data)
-            ScraperThread.threadLock.release()
-
-
-        except common.exceptions.TimeoutException:
-            print('Error:'  + self.url + ' timed out')
-        except common.exceptions.StaleElementReferenceException:
-            print('Error: Could not partse HTML for ' + self.url)
-        finally:
-            self.scraper.driver.close()
-            return
 
 class SearchScraper:
     def __init__(self, driver_location, url):
@@ -69,58 +32,74 @@ class SearchScraper:
         capa = DesiredCapabilities.CHROME
         capa["pageLoadStrategy"] = "normal"
         self.driver = webdriver.Chrome(driver_location, desired_capabilities=capa, chrome_options=chrom_options)
+        self.url = url
 
-        self._open_page(url)
+        self.scraper_functions = [self.get_url,
+                                  self.get_keywords,
+                                  self.get_title,
+                                  self.get_category,
+                                  self.get_links,
+                                  self.get_description]
 
+        self._open_page()
 
-
-    def _open_page(self, url):
+    def _open_page(self):
         self.driver.implicitly_wait(10)
-        if url.startswith('http') or url.startswith('https'):
-            self.url = url
-            self.driver.get(url)
+        if self.url.startswith('http') or self.url.startswith('https'):
+            self.driver.get(self.url)
+            return
         else:
             try:
-                self.driver.get('https://' + url)
-                self.url = 'https://' + url
+                self.driver.get('https://' + self.url)
+                self.url = 'https://' + self.url
             except:
-                self.__init__(self.driver, 'http://' + url)
-                self.url = 'http://' + url
+                self.__init__(self.driver, 'http://' + self.url)
+                self.url = 'http://' + self.url
 
-    def get_keywords (self):
-        page = self.driver.find_element_by_tag_name("html").text
-        page = page.split()
-        object = {}
-       # for word in page_content:
-            # if there's a non-character ignore the word  (^[a-zA-Z]+)
-            # STORE IT INTO AN OBJECT SO I CAN DO getUrlPriority and then we can store into database
-        # getUrlPriority
-        return page
+    def scrape_all(self):
+        website_data = multithread_scraper.scraper_data.PageData()
+        for website_data_funcs in self.scraper_functions:
+            data_piece = website_data_funcs()
+            type_of_data = data_piece[0]
+            data = data_piece[1]
+            website_data.add_data(type_of_data, data)
+
+        return website_data
+
+    def get_keywords(self):
+        all_words = self.driver.find_element_by_tag_name("html").text
+        all_words = all_words.split()
+        key_words = []
+        for word in all_words:
+            if re.fullmatch('^[a-zA-z]+', word) is not None:
+                key_words.append(word)
+        return 'Words', all_words
 
     def get_links(self):
         links = self.driver.find_elements_by_tag_name('a')
         link_list = []
         for sub_domain in links:
-            link_list.append(sub_domain.get_attribute("href"))
+            if (sub_domain is not None):
+                link_list.append(sub_domain.get_attribute("href"))
 
-        return (link_list)
+        return 'SubDomains', link_list
 
     def get_title(self):
         title = self.driver.title
-        return title
+        return 'Title', [title]
 
     def get_category(self):
         meta = self.driver.find_element_by_tag_name("meta")
-        print(meta)
+        return 'Category', [meta]
 
-    def get_description (self):
+    def get_description(self):
         getElem = [(self.driver.find_element_by_name, ('description')),
                    (self.driver.find_element_by_name, ('Description'))]
 
         description = None
         for (func, param) in getElem:
             try:
-                description = func(param).get_attribute('content')
+                description = func(param).get_attribute('content')  # bottleneck
             except common.exceptions.NoSuchElementException:
                 continue
             except common.exceptions.StaleElementReferenceException:
@@ -128,7 +107,7 @@ class SearchScraper:
 
         if description is None:
             description = "null"
-        return [description]
+        return 'Description', [description]
 
-
-
+    def get_url(self):
+        return 'URL', [self.url]
