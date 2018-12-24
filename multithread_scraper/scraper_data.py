ba30ac7e6129814cfd8009c9539c8ffe96f8b249
Trevor Xander <trevorcolexander@gmail.com>
2018-12-24 14:08:04 +0530

    Removed PageData class

--- multithread_scraper/scraper_data.py
+++ multithread_scraper/scraper_data.py
@@ -33,42 +33,19 @@ class UrlList:
             raise StopIteration
 
 
-class PageData:
-    def __init__(self, ):
-        self._data = {}
-        self._data_index = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self._data_index > len(self._data) - 1:
-            raise StopIteration
-        new_data = self._data[self._data_index]
-        self._data_index += 1
-        return new_data
-
-    def add_data(self, data_desc: str, data: list):
-        self._data[data_desc] = data
-
-    def get_data(self, type: str):
-        return self._data[type]
-
-
 class PageDataCollection:
     _MAX_PAGE_DATA = 50
+    _NULL_WORD = "Not Found"
     mutex = Lock()
 
     def __init__(self, write_file_loc):
-        self.key_word_id = 1
-        self.website_id = 1
         self._buffer = queue.Queue(maxsize=PageDataCollection._MAX_PAGE_DATA)
         self._file = open(write_file_loc, 'w')
 
     def __del__(self):
         self._file.close()
 
-    def add_page(self, page_data: PageData):
+    def add_page(self, page_data: dict):
         PageDataCollection.mutex.acquire()
         try:
             self._buffer.put(page_data)
@@ -78,16 +55,15 @@ class PageDataCollection:
             PageDataCollection.mutex.release()
 
     def flush(self):
-        write_count = 0
         while not self._buffer.empty():
-            page_data: PageData
+            page_data: dict
             page_data = self._buffer.get()
 
-            self._file.writelines(page_data._data.get('Title') + ['\n'])
-            self._file.writelines(page_data._data.get('URL') + ['\n'])
-            self._file.writelines(page_data._data.get('Description') + ['\n'])
-
-            words = page_data._data.get('Words')
-            for word in words:
-                self._file.write(word + ' ')
+            for type, data in page_data.items():
+                self._file.write(type)
+                self._file.write(": ")
+                for elem in data:
+                    self._file.writelines(elem)
+                    self._file.write(" ")
+                self._file.write("\n")
             self._file.write('\n----------\n')
