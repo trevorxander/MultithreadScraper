88691c77c4da61fd6427ba5ee9a867418588b3c3
Trevor Xander <trevorcolexander@gmail.com>
2018-12-25 07:56:28 +0530

    Refractor code

---
+++ multithread_scraper/__init__.py
@@ -0,0 +1,4 @@
+from multithread_scraper.scraper_thread import MultithreadScraper
+from multithread_scraper.scraper_data import PageDataCollection
+from multithread_scraper.scraper_data import UrlList
+from multithread_scraper.scraper import SearchScraper
--- multithread_scraper/__main__.py
+++ multithread_scraper/__main__.py
@@ -1,4 +1,7 @@
 from multithread_scraper import scraper_thread
 
 if __name__ == "__main__":
-    scraper = scraper_thread.MultithreadScraper(max_threads=20)
+    scraper = scraper_thread.MultithreadScraper(max_threads=30,
+                                                input='../data/top_websites.txt',
+                                                output='../data/page_data.txt',
+                                                driver='../drivers/chromedriver')
